7df5d0abb753ec0fcdc4f865bcca8f6fc46bb4d1
humberto <humbertorivero9@gmail.com>
2021-06-12 02:42:33 -0300

    agregando componente button y styled component

--- breaking-bad/src/App.js
+++ breaking-bad/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+
+const Contenedor = styled.div`
+  display: flex;
+  padding-top: 5rem;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid #000;
+`;
 
 function App() {
+
+  const consultarApi = () => {
+    console.log('consultando....')
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+      <Boton
+        onClick={() => consultarApi()}
+      >
+        Obtener Frase
+      </Boton>
+    </Contenedor>
   );
 }
 
